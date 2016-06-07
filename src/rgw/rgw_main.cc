a1fdca76feba3a5eb05cdc03e75057f1f099ac6e
root <rahul.1aggarwal@gmail.com>
2016-06-07 12:40:45 +0530

    Squashed commit of the following:
    commit eb075aae5ce6fd8b0170db00058cc38bb9bdc557
    Author: root <rahul.1aggarwal@gmail.com>
    Date:   Wed May 25 18:43:05 2016 +0530
    
        Adding back comment
    
    commit ccbefbe6e5b9b8954ccb4eb16aa046e48eda90cc
    Author: root <rahul.1aggarwal@gmail.com>
    Date:   Wed May 25 16:33:46 2016 +0530
    
        JOS-98 : RGW crashes when DELETE is done concurrently
        - providing threading functions for openssl to support multithreading

--- src/rgw/rgw_main.cc
+++ src/rgw/rgw_main.cc
@@ -1,4 +1,4 @@
-// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*- 
 // vim: ts=8 sw=2 smarttab
 
 #include <stdlib.h>
@@ -71,7 +71,72 @@
 #include "include/types.h"
 #include "common/BackTrace.h"
 
+#include <stdio.h>
+#include <pthread.h>
+#include <openssl/crypto.h>
+
+
 #define dout_subsys ceph_subsys_rgw
+
+
+#define MUTEX_TYPE       pthread_mutex_t
+#define MUTEX_SETUP(x)   pthread_mutex_init(&(x), NULL)
+#define MUTEX_CLEANUP(x) pthread_mutex_destroy(&(x))
+#define MUTEX_LOCK(x)    pthread_mutex_lock(&(x))
+#define MUTEX_UNLOCK(x)  pthread_mutex_unlock(&(x))
+#define THREAD_ID        pthread_self()
+
+
+
+
+/* This array will store all of the mutexes available to OpenSSL. */
+static MUTEX_TYPE *mutex_buf= NULL;
+
+static void openssl_locking_function(int mode, int n, const char * file, int line)
+{
+  if(mode & CRYPTO_LOCK)
+    MUTEX_LOCK(mutex_buf[n]);
+  else
+    MUTEX_UNLOCK(mutex_buf[n]);
+}
+
+static unsigned long openssl_id_function(void)
+{
+    return ((unsigned long)THREAD_ID);
+}
+
+int openssl_thread_setup(void)
+{
+  int i;
+
+  mutex_buf = (MUTEX_TYPE *) malloc(CRYPTO_num_locks() * sizeof(MUTEX_TYPE));
+  if(!mutex_buf)
+    return 0;
+  for(i = 0;  i < CRYPTO_num_locks();  i++)
+    MUTEX_SETUP(mutex_buf[i]);
+  CRYPTO_set_id_callback(openssl_id_function);
+  CRYPTO_set_locking_callback(openssl_locking_function);
+  return 1;
+}
+
+int openssl_thread_cleanup(void)
+{
+  int i;
+
+  if(!mutex_buf)
+    return 0;
+  CRYPTO_set_id_callback(NULL);
+  CRYPTO_set_locking_callback(NULL);
+  for(i = 0;  i < CRYPTO_num_locks();  i++)
+    MUTEX_CLEANUP(mutex_buf[i]);
+  free(mutex_buf);
+  mutex_buf = NULL;
+  return 1;
+}
+
+
+
+
 
 using namespace std;
 
@@ -1174,6 +1239,9 @@ int main(int argc, const char **argv)
   rgw_init_resolver();
   
   curl_global_init(CURL_GLOBAL_ALL);
+
+  /* setup openssl multi-threading functions */
+  openssl_thread_setup();
   
   FCGX_Init();
 
@@ -1366,6 +1434,9 @@ int main(int argc, const char **argv)
   rgw_tools_cleanup();
   rgw_shutdown_resolver();
   curl_global_cleanup();
+
+  /* cleanup openssl multi-threading functions */
+  openssl_thread_cleanup();
 
   rgw_perf_stop(g_ceph_context);
 
